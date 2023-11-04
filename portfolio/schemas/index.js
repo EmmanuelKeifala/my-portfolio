4db0de8765b6a92fdf6c0350c6fd54939568c71c
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-11-04 06:09:51 +0000

    sanity installed and setup with all the schemas

---
+++ portfolio/schemas/index.js
@@ -0,0 +1,19 @@
+import works from './works';
+import testimonials from './testimonials';
+import brands from './brands';
+import abouts from './abouts';
+import experiences from './experiences';
+import workExperience from './workExperience';
+import contact from './contact';
+import skill from './skill';
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  experiences,
+  skill,
+  workExperience,
+  contact,
+];
