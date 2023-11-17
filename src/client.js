30c8c6415d16c5bb9fa8d0e74737f41a6b0ec737
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-11-17 08:14:27 +0000

    the navbar was optimized and animated, and was made to conditionally render

--- src/client.js
+++ src/client.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from '@sanity/image-url';
 export const client = sanityClient({
   projectId: '5e8eqopg',
   dataset: 'production',
-  // apiVersion: '2022-02-01',
+  apiVersion: '2022-02-01',
   useCdn: true,
   token:
     'skdKI830dn3aJcgeCIZoWMLuShHHgjlx8Va7JIgcWZSuG08mabY7GnDwiUFcLrMXAKrLHjrQwqnAsbZoEP5yUgLO5aukem0dHz9Aqdil9sckvNxu9yOMr4z1EimnfaRmwvC5WbANxCojjqOGf3fcQY07DADWugoxWtcmhkRvj6r6cqszaW7Y',
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,8 +20,30 @@ const CustomLinks = ({href, title, className = ''}) => {
     </Link>
   );
 };
+
+const CustomMobileLinks = ({href, title, className = '', toggle}) => {
+  const router = useRouter();
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+  return (
+    <button
+      href={href}
+      className={`${className} relative group text-light my-2`}
+      onClick={handleClick}>
+      {title}
+      <span
+        className={`h-[1px] inline-block bg-light absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? 'w-full' : 'w-0'
+        }`}>
+        &nbsp;
+      </span>
+    </button>
+  );
+};
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
@@ -31,18 +53,19 @@ const Navbar = () => {
         className=" flex-col justify-center items-center hidden lg:flex"
         onClick={handleClick}>
         <span
-          className={`bg-dark dark:bg-light block  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+          className={`bg-dark block  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
             isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'
           }`}></span>
         <span
-          className={`bg-dark dark:bg-light block  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+          className={`bg-dark  block  transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
             isOpen ? 'opacity-0' : 'opacity-100'
           }`}></span>
         <span
-          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+          className={`bg-dark  block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
             isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'
           }`}></span>
       </button>
+
       <div className="w-full flex justify-between items-center lg:hidden">
         <nav className="flex items-center justify-between gap-10 flex-wrap">
           <CustomLinks href={'/'} title="Home" />
@@ -78,41 +101,60 @@ const Navbar = () => {
           </motion.a>
         </nav>
       </div>
-      <div className="min-w-[70vw]  flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2">
-        <nav className="flex items-center justify-between gap-10 flex-wrap">
-          <CustomLinks href={'/'} title="Home" />
-          <CustomLinks href={'/about'} title="About" />
-          <CustomLinks href={'/projects'} title="Projects" />
-          <CustomLinks href={'/articles'} title="Articles" />
-        </nav>
-        <nav className="flex items-center justify-center flex-wrap gap-5 text-3xl">
-          <motion.a
-            href="https://twitter.com/Emmanuel_Kel_"
-            target={'_blank'}
-            whileHover={{y: -2}}
-            whileTap={{scale: 0.9}}
-            className="w-6">
-            <TwitterIcon />
-          </motion.a>
-          <motion.a
-            href="https://github.com/EmmanuelKeifala"
-            target={'_blank'}
-            whileHover={{y: -2}}
-            whileTap={{scale: 0.9}}
-            className="w-6">
-            <GithubIcon />
-          </motion.a>
-          <motion.a
-            href={'/'}
-            target={'_blank'}
-            title="LinkedIn"
-            whileHover={{y: -2}}
-            whileTap={{scale: 0.9}}
-            className="w-6">
-            <LinkedInIcon />
-          </motion.a>
-        </nav>
-      </div>
+
+      {isOpen ? (
+        <motion.div
+          className="min-w-[70vw]  flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 bg-dark/90 rounded-lg backdrop-blur-md py-32"
+          initial={{scale: 0, opacity: 0, x: '-50%', y: '-50%'}}
+          animate={{scale: 1, opacity: 1}}
+          transition={{duration: 0.3}}>
+          <nav className="flex items-center flex-col justify-center">
+            <CustomMobileLinks href={'/'} title="Home" toggle={handleClick} />
+            <CustomMobileLinks
+              href={'/about'}
+              title="About"
+              toggle={handleClick}
+            />
+            <CustomMobileLinks
+              href={'/projects'}
+              title="Projects"
+              toggle={handleClick}
+            />
+            <CustomMobileLinks
+              href={'/articles'}
+              title="Articles"
+              toggle={handleClick}
+            />
+          </nav>
+          <nav className="flex items-center justify-center gap-8 flex-wrap mt-2">
+            <motion.a
+              href="https://twitter.com/Emmanuel_Kel_"
+              target={'_blank'}
+              whileHover={{y: -2}}
+              whileTap={{scale: 0.9}}
+              className="w-6">
+              <TwitterIcon />
+            </motion.a>
+            <motion.a
+              href="https://github.com/EmmanuelKeifala"
+              target={'_blank'}
+              whileHover={{y: -2}}
+              whileTap={{scale: 0.9}}
+              className="w-6 bg-light rounded-full">
+              <GithubIcon />
+            </motion.a>
+            <motion.a
+              href={'/'}
+              target={'_blank'}
+              title="LinkedIn"
+              whileHover={{y: -2}}
+              whileTap={{scale: 0.9}}
+              className="w-6">
+              <LinkedInIcon />
+            </motion.a>
+          </nav>
+        </motion.div>
+      ) : null}
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
